ea94641eb0dbace7363edd6a217788e75b892217
KadDenuwara <kad.denuwara@gmail.com>
2024-07-09 05:33:34 +0530

    delete files

--- newsletter/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import newsletter_signup, newsletter_success
-
-urlpatterns = [
-    path('signup/', newsletter_signup, name='newsletter_signup'),
-    path('success/', newsletter_success, name='newsletter_success'),
-]
